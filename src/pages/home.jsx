7c393c12240f907b6562c3e7ac1d752d62f92ca8
dennisko <dennis0324@naver.com>
2024-05-30 18:38:55 +0900

    add: SearchClient Component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,16 @@
+import SearchClient from "../components/SearchClient";
 function Home() {
-  return <div className="w-lvw h-lvh dot-grid bg-[0 0 / 20px 20px]"></div>;
+  return (
+    <div className="w-lvw h-lvh ">
+      <div className="w-full h-full flex justify-center fixed z-10">
+        <div className="relative">
+          <SearchClient className="absolute top-5 left-1/2 -translate-x-1/2 " />
+        </div>
+        <div>{/*devices information is going to go*/}</div>
+      </div>
+      <div className="dot-grid bg-[0 0 / 20px 20px] fixed w-full h-full z-0"></div>
+    </div>
+  );
 }
 
 export default Home;
