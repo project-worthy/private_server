0c73e7537e229744d442d120b86f3d6db9316e58
dennisko <dennis0324@naver.com>
2024-09-29 13:01:28 +0900

    temp

--- tests/utils/date.test.ts
+++ tests/utils/date.test.ts
@@ -1,4 +1,4 @@
-import { getSelectRange, isActiveTimeIntersect } from "../../src/utils/date";
+import { getSelectRange, isActiveTimeIntersectAll } from "../../src/utils/date";
 const ranges = [
   { key: "", start: 5, end: 10 },
   { key: "", start: 15, end: 20 },
@@ -33,16 +33,18 @@ describe("util date getRange test", () => {
 
 describe("util get isActiveTimeIntersect", () => {
   test("타겟 범위가 범위 배열의 요소를 포함하는 경우", () => {
-    expect(isActiveTimeIntersect({ start: 5, end: 10 }, ranges)).toBe(true);
+    expect(isActiveTimeIntersectAll({ start: 5, end: 10 }, ranges)).toBe(true);
   });
   test("타겟 범위가 범위 배열의 요소 하나를 포함하는 경우", () => {
-    expect(isActiveTimeIntersect({ start: 7, end: 14 }, ranges)).toBe(true);
+    expect(isActiveTimeIntersectAll({ start: 7, end: 14 }, ranges)).toBe(true);
   });
   test("타겟 범위가 범위 배열의 요소 하나를 포함하는 경우", () => {
-    expect(isActiveTimeIntersect({ start: 1, end: 3 }, ranges)).toBe(false);
+    expect(isActiveTimeIntersectAll({ start: 1, end: 3 }, ranges)).toBe(false);
   });
   test("타겟 범위가 범위 배열의 요소 하나를 포함하는 경우", () => {
-    expect(isActiveTimeIntersect({ start: 12, end: 13 }, ranges)).toBe(false);
+    expect(isActiveTimeIntersectAll({ start: 12, end: 13 }, ranges)).toBe(
+      false,
+    );
   });
   // test("타겟 범위가 범위 배열의 요소 하나를 포함하는 경우", () => {
   //   expect(isActiveTimeIntersect({ start: 0, end: 15 }, ranges)).toBe(true);
